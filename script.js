c72dcbcbada9213c0f467c5274716db35be5c3ce
Mahdi <mahdihs395@gmail.com>
2022-07-21 12:19:10 +0300

    refactored the code

--- script.js
+++ script.js
@@ -1,115 +1,157 @@
-const addBtn = document.querySelector('button.add');
-const removeBtn = document.querySelector('button.remove');
-const changeBtn = document.querySelector('button.change');
-const formContainer = document.querySelector('.form-container');
-const form = document.querySelector('.book-form');
-const formBtn = document.querySelector('.form-btn');
-const main = document.querySelector('.main');
-const sideTitle = document.querySelector('.side .title span');
-const sideAuthor = document.querySelector('.side .author span');
-const sidePages = document.querySelector('.side .pages span');
-const sideStatus = document.querySelector('.side .status span');
+const library = [];
+const booksElements = [];
 
-let booksObjects = [];
-let booksElements = [];
-let bookIndex;
 
-function displayForm(visibility, scale){
-  formContainer.setAttribute('style', `visibility:${visibility}`);
-  form.setAttribute('style', `transform:scale(${scale})`)
-}
+const bindEvents = (function(){
+  const addBtn = document.querySelector('.add');
+  const removeBtn = document.querySelector('.remove');
+  const formBtn = document.querySelector('.form-btn');
+  const changeStatusBtn = document.querySelector('.changeStatus');
+  
+  addBtn.addEventListener('click', () => {
+    form.showForm();
+  });
+  
+  formBtn.addEventListener('click', () => {
+    form.hideForm();
+    add();
+  })
 
-function createElement(){
-  main.innerHTML = '';
-  booksElements = [];
+  removeBtn.addEventListener('click', () => {
+    side.clearSide();
+    remove()
+  })
 
-  for(let i = 0; i < booksObjects.length; i++){
+  changeStatusBtn.addEventListener('click', () => {
+    changeStatus();
+    side.renderSide(library[findSelectedBook()]);
+  })
+})()
 
-    let div = document.createElement('div');
-    div.classList.add('card');
-    div.textContent = booksObjects[i].title;
-    if(i === booksObjects.length - 1){
-      div.setAttribute('style', 'border: 3px solid #a3282c');
-    }
-    
-    main.appendChild(div);
-    booksElements.push(div);
+const form = (function(){
+  const formContainer = document.querySelector('.form-container');
+  const bookForm = document.querySelector('.book-form');
+
+  function showForm(){
+    formContainer.style.visibility = 'visible';
+    bookForm.style.transform = 'scale(1)'
   }
-}
 
-function setSideInfo(book){
-  if(book){
-    sideTitle.textContent = book.title;
-    sideAuthor.textContent = book.author;
-    sidePages.textContent = book.pages;
-    sideStatus.textContent = book.status;
-  }else{
+  function hideForm(){
+    formContainer.style.visibility = 'hidden';
+    bookForm.setAttribute('style', 'transform:scale(0)');
+  }
+
+  return {
+    showForm: showForm,
+    hideForm: hideForm,
+  }
+})()
+
+const side = (function(){
+  let sideTitle = document.querySelector('.title span');
+  let sideAuthor = document.querySelector('.author span');
+  let sidePages = document.querySelector('.pages span');
+  let sideStatus = document.querySelector('.status span');
+
+  function clearSide(){
     sideTitle.textContent = '';
     sideAuthor.textContent = '';
     sidePages.textContent = '';
     sideStatus.textContent = '';
   }
+
+  function renderSide(book){
+    sideTitle.textContent = book.title;
+    sideAuthor.textContent = book.author;
+    sidePages.textContent = book.pages;
+    if(book.status === true){
+      sideStatus.textContent = 'Read';
+    }else{
+      sideStatus.textContent = 'Not Read';
+    }
+  }
+
+  return{clearSide, renderSide};
+})()
+
+function add(){
+  const book = BookFactory();
+
+  library.push(book);
+  side.renderSide(book);
+  renderBooks();
 }
 
-function selectBook(){
-  booksElements.forEach((element) => {
-    element.addEventListener('click', () => {
-      
-      for(let i = 0; i < booksElements.length; i++){
-        booksElements[i].setAttribute('style', 'border:none');
-      }
-      element.setAttribute('style', 'border:3px solid #a3282c');
-  
-      bookIndex = booksElements.indexOf(element);
-      sideTitle.textContent = booksObjects[bookIndex].title;
-      sideAuthor.textContent = booksObjects[bookIndex].author;
-      sidePages.textContent = booksObjects[bookIndex].pages;
-      sideStatus.textContent = booksObjects[bookIndex].status;
+function BookFactory(){
+  const book = {};
+  book.title = document.querySelector('#title').value;;
+  book.author = document.querySelector('#author').value;
+  book.pages = document.querySelector('#pages').value;
+  book.status = document.querySelector('#status').checked;
+  return book;
+}
 
+function renderBooks(){
+  const bookContainer = document.querySelector('.main');
+  bookContainer.innerHTML = ''
+
+  for(let book of library){
+    const bookEle = createBookElement();
+    bookEle.textContent = book.title;
+    bookContainer.appendChild(bookEle);
+  }
+  select()
+}
+
+function createBookElement(){
+  const bookEle = document.createElement('div');
+  styleBookElement(bookEle);
+  booksElements.push(bookEle);
+  return bookEle;
+}
+
+function styleBookElement(element){
+  for(let book of booksElements){
+    book.classList.remove('selected');
+  }
+  element.classList.add('book', 'selected')
+}
+
+//created a function so books get updated when an new book is added//
+function select(){
+  const books = document.querySelectorAll('.book');
+  books.forEach(element => {
+    element.addEventListener('click', () => {
+      let index = Array.from(books).indexOf(element);
+      side.renderSide(library[index]);
+      styleBookElement(element);
     })
   })
 }
 
+function remove(){
+  let index = findSelectedBook();
+  library.splice(index,1);
+  renderBooks();
+}
 
-formBtn.addEventListener('click', () => {
-
-  function newBook(){
-    this.title = document.querySelector('#title').value;
-    this.author = document.querySelector('#author').value;
-    this.pages = document.querySelector('#pages').value;
-    this.status = (document.querySelector('#status').checked)?'Read': 'Not read';
+function changeStatus(){
+  let index = findSelectedBook();
+  if(library[index].status === true){
+    library[index].status = false;
+  }else if(library[index].status === false){
+    library[index].status = true;
   }
-  
-  let book = new newBook();
-  booksObjects.push(book);
-  bookIndex = booksObjects.length - 1;
+}
 
-  createElement();
-  setSideInfo(book);
-  displayForm('hidden', 0);
-  selectBook();
-  
-})
-
-addBtn.addEventListener('click', () => {
-  displayForm('visible', 1)
-})
-
-removeBtn.addEventListener('click', () => {
-  booksObjects.splice(bookIndex, 1);
-  createElement();
-  setSideInfo();
-  selectBook();
-})
-
-changeBtn.addEventListener('click', () => {
-  if(bookIndex !== undefined){
-    if(booksObjects[bookIndex].status === 'Read'){
-      booksObjects[bookIndex].status = 'Not read';
-      setSideInfo(booksObjects[bookIndex])
-    }else{
-      booksObjects[bookIndex].status = 'Read';
-      setSideInfo(booksObjects[bookIndex])
+function findSelectedBook(){
+  const books = document.querySelectorAll('.book');
+  let index;
+  for(let i of books){
+    if(i.classList.contains('selected')){
+      index = Array.from(books).indexOf(i)
     }
   }
-})
+  return index
+};
